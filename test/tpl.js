45cf6366b91698b7466f4cb9e485e1adcb0bd92f
airyland <airyland@qq.com>
2014-09-12 06:30:24 +0800

    fix single quote marks missing

--- test/tpl.js
+++ test/tpl.js
@@ -7,8 +7,8 @@ describe('tpl', function() {
   it('normal', function(done) {
 
     var origin = '{{a}}\n\r\'\"';
-    var expected = util.define('module.exports = ' +
-      '{{a}}\"\";');
+    var expected = util.define('module.exports = \'' +
+      '{{a}}\"\"\';');
 
     var stream = tplParser();
     stream.on('data', function(newFile) {
