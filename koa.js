e906fbf19e01374600d885407743c07470f441b0
popomore <sakura9515@gmail.com>
2014-10-30 20:28:45 +0800

    feat(middleware) koa support
    Closes #6

---
+++ koa.js
@@ -0,0 +1,131 @@
+var path = require('path');
+var fs = require('fs');
+var urlparse = require('url').parse;
+var through = require('through2');
+var pipe = require('multipipe');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var gulpif = require('gulp-if');
+var coffee = require('gulp-coffee');
+var mime = require('mime');
+var extend = require('extend');
+
+var cssParser = require('./parser/css');
+var css2jsParser = require('./parser/css2js');
+var jsParser = require('./parser/js');
+var tplParser = require('./parser/tpl');
+var jsonParser = require('./parser/json');
+var handlebarsParser = require('./parser/handlebars');
+var standalonify = require('./parser/standalonify');
+var Parser = require('./parser');
+var util = require('./util');
+
+
+module.exports = function(root, opts) {
+  opts = opts || {};
+  return function*(next) {
+    if (this.header['servespmexit']) {
+      next = notFound(this);
+    }
+
+    var pkg = util.getPkg(root);
+    if (!pkg) {
+      return yield* next;
+    }
+
+    var parser = new Parser(extend({
+      root: root,
+      req: urlparse(this.url.toLowerCase()),
+      headers: this.header,
+      pkg: util.getPkg(root)
+    }, opts));
+
+    // is dep package, but not found
+    if (!parser.pkg) {
+      return yield* next;
+    }
+
+    // don't handle if file is not found
+    if (!parser.file) {
+      return yield* next;
+    }
+
+    // don't handle dir
+    if (parser.isDir) {
+      return yield* next;
+    }
+
+    // 304
+    var isModified = parser.isModified();
+    this.set('Last-Modified', parser.modifiedTime);
+    if (!isModified) {
+      this.status = 304;
+      return;
+    }
+
+    if (opts.log) {
+      console.log('>> ServeSPM %s < ./%s',
+        parser.req.pathname, path.relative(process.cwd(), parser.file));
+    }
+
+    var data, ext;
+    // nowrap
+    if (parser.noWrap) {
+      data = fs.readFileSync(parser.file, 'utf-8');
+      if (parser.handlebarId) {
+        data = util.template(data, {id:parser.handlebarId});
+      }
+      ext = path.extname(parser.file);
+    }
+
+    else {
+      // transport file
+      var file = yield parse(parser);
+      data = file.contents;
+      ext = path.extname(file.path);
+    }
+
+    if (['.tpl', '.json', '.handlebars'].indexOf(ext) > -1) {
+      ext = '.js';
+    }
+    this.set('Content-Type', mime.lookup(ext));
+    this.body = data;
+    this.status = 200;
+  };
+};
+
+module.exports.util = util;
+
+function* notFound(ctx) {
+  ctx.status = 404;
+}
+
+function parse(parser) {
+  var file = parser.file;
+  var useCss2jsParser = util.isCSSFile(file) &&
+    /\.js$/.test(parser.req.pathname);
+  var useStandalone = function(file) {
+    return parser.isStandalone(file.path);
+  };
+
+  return function(cb) {
+    pipe(
+      gulp.src(file),
+
+      gulpif(/\.less$/, less({ paths: [path.dirname(file)] })),
+      gulpif(/\.css$/, cssParser(parser)),
+      gulpif(useCss2jsParser, css2jsParser(parser)),
+      gulpif(/\.coffee$/, coffee({bare: true})),
+      gulpif(/\.js$/, jsParser(parser)),
+      gulpif(/\.tpl$/, tplParser(parser)),
+      gulpif(/\.json$/, jsonParser(parser)),
+      gulpif(/\.handlebars$/, handlebarsParser(parser)),
+      gulpif(useStandalone, standalonify(this.url)),
+
+      through.obj(function(file) {
+        cb(null, file);
+      })
+    )
+    .once('error', cb);
+  };
+}
--- test/index.js
+++ test/index.js
@@ -1,190 +1,210 @@
 var join = require('path').join;
-var serveSPM = require('../');
 var should = require('should');
-var http = require('http');
 var request = require('supertest');
-var util = require('../util');
 var spy = require('spy');
+var util = require('../util');
 var express = require('express');
-
-var app;
+var expressMiddleware = require('../');
+var koa = require('koa');
+var koaMiddleware = require('../koa');
 var root = join(__dirname, 'fixtures/parser');
 
-describe('index', function() {
-
-  before(function() {
-    app = http.createServer(serveSPM(root));
-  });
-
-  it('normal', function(done) {
-    request(app.listen())
-    .get('/index.js')
-    .expect(util.define('var b = require("b/0.1.0/index.js");\nconsole.log(\'a\');\n'))
-    .expect(200, done);
-  });
-
-  it('transportId only in root entries and with id_leading prefix request', function(done) {
-    var id = 'a/0.1.0/index.js';
-    request(app.listen())
-    .get('/' + id)
-    .expect(util.define('var b = require("b/0.1.0/index.js");\nconsole.log(\'a\');\n', id))
-    .expect(200, done);
-  });
-
-  it('require pkg file in js', function(done) {
-    request(app.listen())
-    .get('/pkg-file.js')
-    .expect(util.define('require("b/0.1.0/path/to/file");\n'))
-    .expect(200, done);
-  });
-
-  it('require pkg file in js (devDependencies)', function(done) {
-    request(app.listen())
-    .get('/pkg-file-dev.js')
-    .expect(util.define('require("c/0.1.0/path/to/file");\n'))
-    .expect(200, done);
-  });
-
-  it('import pkg file in css', function(done) {
-    request(app.listen())
-    .get('/pkg-file.css')
-    .expect('@import "/b/0.1.0/a/b.css";\n')
-    .expect(200, done);
-  });
-
-  it('import pkg file in css (devDependencies)', function(done) {
-    request(app.listen())
-    .get('/pkg-file-dev.css')
-    .expect('@import "/c/0.1.0/a/b.css";\n')
-    .expect(200, done);
-  });
-
-  it('nowrap', function(done) {
-    request(app.listen())
-    .get('/index.js?nowrap')
-    .expect('var b = require(\'b\');\nconsole.log(\'a\');\n')
-    .expect(200, done);
-  });
-
-  it('dep pkg', function(done) {
-    request(app.listen())
-    .get('/b/0.1.0/index.js')
-    .expect(util.define('console.log(\'b\');\n'))
-    .expect(200, done);
-  });
-
-  it('dep pkg but not found', function(done) {
-    request(app.listen())
-    .get('/notfound/0.1.0/index.js')
-    .expect(404, done);
-  });
-
-  it('file not found', function(done) {
-    request(app.listen())
-    .get('/notfound.js')
-    .expect(404, done);
-  });
-
-  it('handlebars', function(done) {
-    request(app.listen())
-    .get('/dist/cjs/handlebars.runtime.js')
-    .expect(/^define\("dist\/cjs\/handlebars.runtime"/)
-    .expect(200, done);
-  });
-
-  it('handlebars2', function(done) {
-    request(app.listen())
-    .get('/handlebars-runtime.js')
-    .expect(/^define\("handlebars-runtime"/)
-    .expect(200, done);
-  });
-
-  it('tpl', function(done) {
-    request(app.listen())
-    .get('/g.json')
-    .expect(util.define('module.exports = {"a":1};'))
-    .expect(200, done);
-  });
-
-  it('css.js', function(done) {
-    request(app.listen())
-    .get('/a.css.js')
-    .expect(util.define('seajs.importStyle(\'h1{color:red;}\');'))
-    .expect(200, done);
-  });
-
-  it('isModified', function(done) {
-    request(app.listen())
-    .get('/index.js')
-    .set('if-modified-since', '2046 8-14 13:52:38')
-    .expect(304, done);
-  });
-
-  it('isDir', function(done) {
-    request(app.listen())
-    .get('')
-    .expect(404, done);
-  });
+describe('express', function() {
+  wrap(express, expressMiddleware);
 });
 
-describe('log option', function() {
-  before(function() {
-    app = http.createServer(serveSPM(root, {
-      log: true
-    }));
-  });
+describe('koa', function() {
+  wrap(koa, koaMiddleware);
+});
 
-  it('normal', function(done) {
-    var log = spy(console, 'log');
-    request(app.listen())
-    .get('/index.js')
-    .end(function(err) {
-      should.not.exist(err);
-      log.called.should.be.true;
-      log.restore();
-      done();
+function wrap(server, middleware) {
+  var app;
+
+  describe('index', function() {
+
+    before(function() {
+      app = server();
+      app.use(middleware(root));
+    });
+
+    it('normal', function(done) {
+      request(app.listen())
+      .get('/index.js')
+      .expect(util.define('var b = require("b/0.1.0/index.js");\nconsole.log(\'a\');\n'))
+      .expect(200, done);
+    });
+
+    it('transportId only in root entries and with id_leading prefix request', function(done) {
+      var id = 'a/0.1.0/index.js';
+      request(app.listen())
+      .get('/' + id)
+      .expect(util.define('var b = require("b/0.1.0/index.js");\nconsole.log(\'a\');\n', id))
+      .expect(200, done);
+    });
+
+    it('require pkg file in js', function(done) {
+      request(app.listen())
+      .get('/pkg-file.js')
+      .expect(util.define('require("b/0.1.0/path/to/file");\n'))
+      .expect(200, done);
+    });
+
+    it('require pkg file in js (devDependencies)', function(done) {
+      request(app.listen())
+      .get('/pkg-file-dev.js')
+      .expect(util.define('require("c/0.1.0/path/to/file");\n'))
+      .expect(200, done);
+    });
+
+    it('import pkg file in css', function(done) {
+      request(app.listen())
+      .get('/pkg-file.css')
+      .expect('@import "/b/0.1.0/a/b.css";\n')
+      .expect(200, done);
+    });
+
+    it('import pkg file in css (devDependencies)', function(done) {
+      request(app.listen())
+      .get('/pkg-file-dev.css')
+      .expect('@import "/c/0.1.0/a/b.css";\n')
+      .expect(200, done);
+    });
+
+    it('nowrap', function(done) {
+      request(app.listen())
+      .get('/index.js?nowrap')
+      .expect('var b = require(\'b\');\nconsole.log(\'a\');\n')
+      .expect(200, done);
+    });
+
+    it('dep pkg', function(done) {
+      request(app.listen())
+      .get('/b/0.1.0/index.js')
+      .expect(util.define('console.log(\'b\');\n'))
+      .expect(200, done);
+    });
+
+    it('dep pkg but not found', function(done) {
+      request(app.listen())
+      .get('/notfound/0.1.0/index.js')
+      .expect(404, done);
+    });
+
+    it('file not found', function(done) {
+      request(app.listen())
+      .get('/notfound.js')
+      .expect(404, done);
+    });
+
+    it('handlebars', function(done) {
+      request(app.listen())
+      .get('/dist/cjs/handlebars.runtime.js')
+      .expect(/^define\("dist\/cjs\/handlebars.runtime"/)
+      .expect(200, done);
+    });
+
+    it('handlebars2', function(done) {
+      request(app.listen())
+      .get('/handlebars-runtime.js')
+      .expect(/^define\("handlebars-runtime"/)
+      .expect(200, done);
+    });
+
+    it('tpl', function(done) {
+      request(app.listen())
+      .get('/g.json')
+      .expect(util.define('module.exports = {"a":1};'))
+      .expect(200, done);
+    });
+
+    it('css.js', function(done) {
+      request(app.listen())
+      .get('/a.css.js')
+      .expect(util.define('seajs.importStyle(\'h1{color:red;}\');'))
+      .expect(200, done);
+    });
+
+    it('isModified', function(done) {
+      request(app.listen())
+      .get('/index.js')
+      .set('if-modified-since', '2046 8-14 13:52:38')
+      .expect(304, done);
+    });
+
+    it('isDir', function(done) {
+      request(app.listen())
+      .get('')
+      .expect(404, done);
     });
   });
-});
 
-describe('servespmexit', function() {
-  var ep = express();
-  ep.use(serveSPM(root, {
-    log: true
-  }));
-  ep.use(function(req, res) {
-    res.end('ok');
+  describe('log option', function() {
+    before(function() {
+      app = server();
+      app.use(middleware(root, {
+        log: true
+      }));
+    });
+
+    it('normal', function(done) {
+      var log = spy(console, 'log');
+      request(app.listen())
+      .get('/index.js')
+      .end(function(err) {
+        should.not.exist(err);
+        log.called.should.be.true;
+        log.restore();
+        done();
+      });
+    });
   });
 
-  before(function() {
-    app = http.createServer(ep);
+  describe('servespmexit', function() {
+
+    before(function() {
+      app = server();
+      app.use(middleware(root, {
+        log: true
+      }));
+      if (server === koa) {
+        app.use(function*() {
+          this.body = 'ok';
+        });
+      } else {
+        app.use(function(req, res) {
+          res.end('ok');
+        });
+      }
+
+    });
+
+    it('without servespmexit header', function(done) {
+      request(app.listen())
+      .get('/a/0.1.0/notfound.js')
+      .expect('ok')
+      .expect(200, done);
+    });
+
+    it('with servespmexit header', function(done) {
+      request(app.listen())
+      .get('/a/0.1.0/notfound.js')
+      .set('servespmexit', '1')
+      .expect(404, done);
+    });
   });
 
-  it('without servespmexit header', function(done) {
-    request(app.listen())
-    .get('/a/0.1.0/notfound.js')
-    .expect('ok')
-    .expect(200, done);
-  });
+  describe('root not exist', function() {
+    before(function() {
+      app = server();
+      app.use(middleware(join(root, 'notfound'), {
+        log: true
+      }));
+    });
 
-  it('with servespmexit header', function(done) {
-    request(app.listen())
-    .get('/a/0.1.0/notfound.js')
-    .set('servespmexit', '1')
-    .expect(404, done);
+    it('normal', function(done) {
+      request(app.listen())
+      .get('/index.js')
+      .expect(404, done);
+    });
   });
-});
-
-describe('root not exist', function() {
-  before(function() {
-    app = http.createServer(serveSPM(join(root, 'notfound'), {
-      log: true
-    }));
-  });
-
-  it('normal', function(done) {
-    request(app.listen())
-    .get('/index.js')
-    .expect(404, done);
-  });
-});
+}
